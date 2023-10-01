41a9ff2a82bbf5b78502b98231cd63bcdd8846e5
Jose Daniel Romero <danyrome12@gmail.com>
2023-10-01 11:27:53 -0500

    Add SceneInit component and assets folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
+import { useEffect } from 'react';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import SceneInit from './lib/SceneInit';
+
 function App() {
+  useEffect(() => {
+    const test = new SceneInit('myThreeJsCanvas');
+    test.initialize();
+    test.animate();
+
+    let loadedModel;
+    const glftLoader = new GLTFLoader();
+    glftLoader.load('./assets/maxilary-central/scene.gltf', (gltfScene) => {
+      loadedModel = gltfScene;
+      console.log(loadedModel);
+
+      gltfScene.scene.position.y = -90;
+      gltfScene.scene.position.z = -180;
+
+      gltfScene.scene.scale.set(50, 50, 50);
+      test.scene.add(gltfScene.scene);
+    });
+
+    // const animate = () => {
+    //   if (loadedModel) {
+    //     // loadedModel.scene.rotation.x += 0.01;
+    //     loadedModel.scene.rotation.y += 0.01;
+    //     // loadedModel.scene.rotation.z += 0.01;
+    //   }
+    //   requestAnimationFrame(animate);
+    // };
+    // animate();
+  }, []);
+
   return (
-    <div className='App'>
-      
+    <div>
+      <canvas id="myThreeJsCanvas" />
     </div>
-  )
+  );
 }
 
 export default App;
